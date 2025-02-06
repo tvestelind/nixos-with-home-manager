4732d9d5887e2a5546b29c79252143677163adf9
Tomas Vestelind <tomas.vestelind@gmail.com>
2025-02-06 22:53:10 +0100

    Refactor sorting

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -38,21 +38,21 @@
         cursorColor = "#F5E0DC";
         
         color0 = "#45475A";
-        color8 = "#585B70";
         color1 = "#F38BA8";
-        color9 = "#F38BA8";
-        color2 = "#A6E3A1";
         color10 = "#A6E3A1";
-        color3 = "#F9E2AF";
         color11 = "#F9E2AF";
-        color4 = "#89B4FA";
         color12 = "#89B4FA";
-        color5 = "#F5C2E7";
         color13 = "#F5C2E7";
-        color6 = "#94E2D5";
         color14 = "#94E2D5";
-        color7 = "#BAC2DE";
         color15 = "#A6ADC8";
+        color2 = "#A6E3A1";
+        color3 = "#F9E2AF";
+        color4 = "#89B4FA";
+        color5 = "#F5C2E7";
+        color6 = "#94E2D5";
+        color7 = "#BAC2DE";
+        color8 = "#585B70";
+        color9 = "#F38BA8";
 
         mouseWheelScrollPage = "true";
         selectToClipboard = "true";
