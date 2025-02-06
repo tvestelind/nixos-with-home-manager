f2c0f597b532a05cad4967b54524da5d4d47f27e
Tomas Vestelind <tomas.vestelind@gmail.com>
2025-02-06 23:52:32 +0100

    Make nvim more default and add some plugins

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -92,6 +92,13 @@
     neovim = {
       enable = true;
       defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
+      plugins = with pkgs.vimPlugins; [
+        nvim-lspconfig
+        nvim-treesitter.withAllGrammars
+      ];
     };
   };
 
